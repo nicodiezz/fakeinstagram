5a21debd64eec015024b3b479e4913a24c4b4bcb
nicodiezz <nicolasagusdiez@gmail.com>
2024-05-08 20:14:06 -0300

    start base, home, posts, and users apps

--- fakeinstagram/fakeinstagram/settings.py
+++ fakeinstagram/fakeinstagram/settings.py
@@ -30,7 +30,7 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
-INSTALLED_APPS = [
+BASE_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -38,6 +38,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 ]
+LOCAL_APPS = []
+THIRD_APPS = []
+
+INSTALLED_APPS = BASE_APPS + LOCAL_APPS + THIRD_APPS
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
