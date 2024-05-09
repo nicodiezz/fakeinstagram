2cb2b960309abe3420f3aa005bd5ea1f821007db
nicodiezz <nicolasagusdiez@gmail.com>
2024-05-09 11:18:27 -0300

    Create CustomUser model

--- fakeinstagram/users/models.py
+++ fakeinstagram/users/models.py
@@ -5,20 +5,15 @@ from django.db import models
 # Create your models here.
 class CustomUser(AbstractUser):
     profile_image = models.ImageField(_("profile image"), upload_to="proflie_images", default=None,null=True,blank=True)
-    biography = models.CharField(_("boigraphy"), max_length=100, default=None,null=True,blank=True)
+    biography = models.CharField(_("biography"), max_length=100, default=None,null=True,blank=True)
     website = models.URLField(_("website"), max_length=200, default=None,null=True,blank=True)
-    
-    SEX_CHOICES = (
-        ('M', 'Man'),
-        ('W', 'Woman'),
-        ('O', 'Other'),
-    )
-    sex = models.CharField(max_length=1,choices=SEX_CHOICES,default=None)
     birth_date = models.DateField(_("birth date"), default=None,null=True,blank=True)
-    followed = models.ManyToManyField("users.CustomUser", verbose_name=_("followed"))
-
-    groups = models.ManyToManyField(Group, related_name='customuser_set')
-    user_permissions = models.ManyToManyField(Permission, related_name='customuser_set')
+    followed = models.ManyToManyField("users.CustomUser", verbose_name=_("followed"), blank=True)
+    SEX_CHOICES = (('M', 'Man'),('W', 'Woman'),('O', 'Other'))
+    sex = models.CharField(max_length=1,choices=SEX_CHOICES,default=None)
+    
+    groups = models.ManyToManyField(Group, related_name='customuser_set', blank=True)
+    user_permissions = models.ManyToManyField(Permission, related_name='customuser_set', blank=True)
 
     class Meta:
         verbose_name = _("CustomUser")
