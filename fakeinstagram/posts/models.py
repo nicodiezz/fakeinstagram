97a672e05486b70d6753d0c0c09899e6beb0bd4c
nicodiezz <nicolasagusdiez@gmail.com>
2024-05-09 11:17:59 -0300

    mend
    Create post and like models

--- fakeinstagram/posts/models.py
+++ fakeinstagram/posts/models.py
@@ -1,26 +1,31 @@
 from django.db import models
 from base.models import BaseModel
+from django.utils.translation import gettext_lazy as _
+from django.urls import reverse
 # Create your models here.
 class Like(models.Model):
     post = models.ForeignKey("posts.Post", on_delete=models.CASCADE)
-    user = models.OneToOneField("users.User", on_delete=models.DO_NOTHING)
+    user = models.ForeignKey("users.CustomUser", on_delete=models.DO_NOTHING)
 
     class Meta:
-        verbose_name = "Like"
-        verbose_name_plural = "Likes"
+        verbose_name = _("Like")
+        verbose_name_plural = _("Likes")
 
-    def __str__(self):
-        return self.name
+    def get_absolute_url(self):
+        return reverse("model_detail", kwargs={"pk": self.pk})
+    
     
 class Post(BaseModel):
-    user = models.ForeignKey("users.User", on_delete=models.CASCADE)
-    image = models.ImageField(upload_to='images')
+    user = models.ForeignKey("users.CustomUser", on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='posts')
     description = models.fields.CharField(max_length=256)
     likes = models.IntegerField() 
     
-
     class Meta:
-        verbose_name = "Post"
-        verbose_name_plural = "Posts"
-        def __str__(self):
-            return self.name
+        verbose_name = _("Post")
+        verbose_name_plural = _("Posts")
+    def __str__(self):
+        return f"{self.user.username}'s post"
+    def get_absolute_url(self):
+        return reverse("model_detail", kwargs={"pk": self.pk})
+        
--- fakeinstagram/users/models.py
+++ fakeinstagram/users/models.py
@@ -1,2 +1,31 @@
+from django.contrib.auth.models import AbstractUser, Group,Permission
+from django.utils.translation import gettext_lazy as _
+from django.urls import reverse
 from django.db import models
 # Create your models here.
+class CustomUser(AbstractUser):
+    profile_image = models.ImageField(_("profile image"), upload_to="proflie_images", default=None,null=True,blank=True)
+    biography = models.CharField(_("boigraphy"), max_length=100, default=None,null=True,blank=True)
+    website = models.URLField(_("website"), max_length=200, default=None,null=True,blank=True)
+    
+    SEX_CHOICES = (
+        ('M', 'Man'),
+        ('W', 'Woman'),
+        ('O', 'Other'),
+    )
+    sex = models.CharField(max_length=1,choices=SEX_CHOICES,default=None)
+    birth_date = models.DateField(_("birth date"), default=None,null=True,blank=True)
+    followed = models.ManyToManyField("users.CustomUser", verbose_name=_("followed"))
+
+    groups = models.ManyToManyField(Group, related_name='customuser_set')
+    user_permissions = models.ManyToManyField(Permission, related_name='customuser_set')
+
+    class Meta:
+        verbose_name = _("CustomUser")
+        verbose_name_plural = _("CustomUsers")
+
+    def __str__(self):
+        return self.username
+
+    def get_absolute_url(self):
+        return reverse("CustomUser_detail", kwargs={"pk": self.pk})
