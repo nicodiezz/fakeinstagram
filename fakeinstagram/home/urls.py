0693a16303d4716670248ba8c452fc121f436e63
nicodiezz <nicolasagusdiez@gmail.com>
2024-05-21 17:41:49 -0300

    create and style profile.html and search.html

--- fakeinstagram/home/urls.py
+++ fakeinstagram/home/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views 
 urlpatterns = [
-    path('',views.PostListView.as_view(),name="home")
+    path('',views.PostListView.as_view(),name="home"),
+    path('search/',views.SearchView.as_view(),name="search"),
+    path('profile/',views.ProfileView.as_view(),name="profile")
 ]
--- fakeinstagram/home/views.py
+++ fakeinstagram/home/views.py
@@ -1,8 +1,25 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, TemplateView
 from posts.models import Post
+from users.models import CustomUser
 # Create your views here.
 class PostListView(ListView):
     model = Post
     template_name = "home.html"
     context_object_name = "posts"
+
+class SearchView(TemplateView):
+    template_name = "search.html"
+
+class ProfileView(TemplateView):
+    template_name = "profile.html"
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        user = CustomUser.objects.get(username = "hola")
+        context["user"] =  user
+        context["posts"] =  Post.objects.all().filter(user_id = user.id)
+        return context
+    
+
+
+
