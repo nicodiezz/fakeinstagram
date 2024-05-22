f9592880a4317f085eaf2bed893ca82e28676b64
nicodiezz <nicolasagusdiez@gmail.com>
2024-05-22 17:00:21 -0300

    create comments model, update CustomUser and Post to be like the new class diagram

--- fakeinstagram/posts/models.py
+++ fakeinstagram/posts/models.py
@@ -15,10 +15,25 @@ class Post(BaseModel):
     user = models.ForeignKey("users.CustomUser", on_delete=models.CASCADE)
     image = models.ImageField(upload_to='posts')
     description = models.fields.CharField(max_length=256)
-    likes = models.IntegerField() 
+    likes_count = models.IntegerField(default=0, null=True, blank=True) 
     
     class Meta:
         verbose_name = _("Post")
         verbose_name_plural = _("Posts")
     def __str__(self):
         return f"{self.user.username}'s post"
+
+class Comment(BaseModel):
+    post = models.ForeignKey("posts.Post", on_delete=models.CASCADE)
+    user = models.ForeignKey("users.CustomUser", on_delete=models.CASCADE)
+    text = models.CharField(max_length=256)
+ 
+    class Meta:
+        verbose_name = _("Comment")
+        verbose_name_plural = _("Comments")
+
+    def __str__(self):
+        return self.text
+
+    def get_absolute_url(self):
+        return reverse("Comment_detail", kwargs={"pk": self.pk})
--- fakeinstagram/users/models.py
+++ fakeinstagram/users/models.py
@@ -4,16 +4,17 @@ from django.urls import reverse
 from django.db import models
 # Create your models here.
 class CustomUser(AbstractUser):
-    profile_image = models.ImageField(_("profile image"), upload_to="profile_images", default=None,null=True,blank=True)
+    profile_picture = models.ImageField(_("profile picture"), upload_to="profile_picture", default=None,null=True,blank=True)
     biography = models.CharField(_("biography"), max_length=100, default=None,null=True,blank=True)
     website = models.URLField(_("website"), max_length=200, default=None,null=True,blank=True)
     birth_date = models.DateField(_("birth date"), default=None,null=True,blank=True)
-    followed = models.ManyToManyField("users.CustomUser", verbose_name=_("followed"), blank=True)
-    SEX_CHOICES = (('M', 'Man'),('W', 'Woman'),('O', 'Other'))
-    sex = models.CharField(max_length=1,choices=SEX_CHOICES,default=None)
-    
-    groups = models.ManyToManyField(Group, related_name='customuser_set', blank=True)
-    user_permissions = models.ManyToManyField(Permission, related_name='customuser_set', blank=True)
+    following_count = models.IntegerField(default = None)
+    followers_count = models.IntegerField(default = None) 
+    posts_count = models.IntegerField(default = None)   
+    following = models.ManyToManyField("users.CustomUser", verbose_name=_("following"), blank=True, default = None)
+
+    groups = models.ManyToManyField(Group, related_name='customuser_set', blank=True, default = None)
+    user_permissions = models.ManyToManyField(Permission, related_name='customuser_set', blank=True, default = None)
 
     class Meta:
         verbose_name = _("CustomUser")
