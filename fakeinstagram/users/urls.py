ecc5cbaf92e972260df138b2515f434ea6307277
nicodiezz <nicolasagusdiez@gmail.com>
2024-06-02 00:21:14 -0300

    Implement follow function in user-detail

--- fakeinstagram/users/urls.py
+++ fakeinstagram/users/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path("signup/",views.SignUpView.as_view(),name="signup"),
-    path("profile/<pk>",views.CustomUserDetailView.as_view(),name="user_detail"),
+    path("profile/<int:pk>",views.CustomUserDetailView.as_view(),name="user_detail"),
+    path("profile/follow/<int:pk>", views.FollowView.as_view(),name="follow")
 ]
