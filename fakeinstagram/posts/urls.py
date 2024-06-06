ed951d0f1c6cf46e7c032071f7af1963fd95addb
nicodiezz <nicolasagusdiez@gmail.com>
2024-06-06 12:36:58 -0300

    Create post_detail url

--- fakeinstagram/posts/urls.py
+++ fakeinstagram/posts/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('create/', views.PostCreateView.as_view(), name="create_post")
+    path('create/', views.PostCreateView.as_view(), name="create_post"),
+    path('post/<pk>', views.PostDetailView.as_view(), name="post_detail")
 ]
