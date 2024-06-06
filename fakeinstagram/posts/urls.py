062aaa55d865b0cb0d49334ccbe927c2a583dc53
nicodiezz <nicolasagusdiez@gmail.com>
2024-06-06 17:11:53 -0300

    Add update post url

--- fakeinstagram/posts/urls.py
+++ fakeinstagram/posts/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('create/', views.PostCreateView.as_view(), name="create_post"),
-    path('post/<pk>', views.PostDetailView.as_view(), name="post_detail")
+    path('post/<pk>', views.PostDetailView.as_view(), name="post_detail"),
+    path('edit-post/<pk>', views.PostUpdateView.as_view(), name="update_post"),
 ]
