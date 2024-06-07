f0885bcda39d22dc0f022f862bdeee9d5b73db3f
nicodiezz <nicolasagusdiez@gmail.com>
2024-06-07 18:15:40 -0300

    delete post url

--- fakeinstagram/posts/urls.py
+++ fakeinstagram/posts/urls.py
@@ -4,4 +4,5 @@ urlpatterns = [
     path('create/', views.PostCreateView.as_view(), name="create_post"),
     path('post/<pk>', views.PostDetailView.as_view(), name="post_detail"),
     path('edit-post/<pk>', views.PostUpdateView.as_view(), name="update_post"),
+    path('delete-post/<pk>', views.PostDeleteView.as_view(), name="delete_post"),
 ]
