1297a38ad5bd8d2430ef836f4373836449ed1b5b
nicodiezz <nicolasagusdiez@gmail.com>
2024-06-08 19:26:54 -0300

    Add followed and following listsviews to urls

--- fakeinstagram/users/urls.py
+++ fakeinstagram/users/urls.py
@@ -5,5 +5,7 @@ urlpatterns = [
     path("profile/<int:pk>",views.CustomUserDetailView.as_view(),name="user_detail"),
     path("profile/<int:pk>/follow/", views.FollowView.as_view(),name="follow"),
     path("profile/<int:pk>/unfollow/", views.UnfollowView.as_view(),name="unfollow"),
-    path("edit-profile/",views.CustomUserUpdateView.as_view(),name="edit_profile")
-]
+    path("edit-profile/",views.CustomUserUpdateView.as_view(),name="edit_profile"),
+    path('followed/<pk>',views.FollowedListView.as_view(),name="followed_list"),
+    path('following/<pk>',views.FollowingListView.as_view(),name="following_list"),
+    ]
