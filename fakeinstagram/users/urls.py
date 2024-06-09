334392f9df252ab49b44dd031f172097919e4218
nicodiezz <nicolasagusdiez@gmail.com>
2024-06-08 23:18:01 -0300

    rename followed to followers at urls.py

--- fakeinstagram/users/urls.py
+++ fakeinstagram/users/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path("profile/<int:pk>/follow/", views.FollowView.as_view(),name="follow"),
     path("profile/<int:pk>/unfollow/", views.UnfollowView.as_view(),name="unfollow"),
     path("edit-profile/",views.CustomUserUpdateView.as_view(),name="edit_profile"),
-    path('followed/<pk>',views.FollowedListView.as_view(),name="followed_list"),
+    path('followers/<pk>',views.FollowersListView.as_view(),name="followers_list"),
     path('following/<pk>',views.FollowingListView.as_view(),name="following_list"),
     ]
