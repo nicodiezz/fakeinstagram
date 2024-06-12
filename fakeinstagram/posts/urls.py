3b0310d958b0488006ef8fe177c0a96dc940b26c
nicodiezz <nicolasagusdiez@gmail.com>
2024-06-12 15:12:29 -0300

    Add like_post url

--- fakeinstagram/posts/urls.py
+++ fakeinstagram/posts/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
 from . import views
-urlpatterns = [
+posts = [
     path('create/', views.PostCreateView.as_view(), name="create_post"),
     path('post/<pk>', views.PostDetailView.as_view(), name="post_detail"),
     path('edit-post/<pk>', views.PostUpdateView.as_view(), name="update_post"),
     path('delete-post/<pk>', views.PostDeleteView.as_view(), name="delete_post"),
 ]
+likes=[
+    path('like/<pk>', views.LikeCreateView.as_view(), name="like_post"),
+]
+
+urlpatterns= posts+likes
