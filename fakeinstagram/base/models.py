2e0154f1bf5db621713706fa92a900eb3a6cac02
nicodiezz <nicolasagusdiez@gmail.com>
2024-06-12 16:40:45 -0300

    Fix error at BaseModel auto_now -> auto_now_add

--- fakeinstagram/base/models.py
+++ fakeinstagram/base/models.py
@@ -1,9 +1,9 @@
 from django.db import models
-
+from datetime import datetime
 # Create your models here.
 class BaseModel(models.Model):
-    created_at = models.DateField(auto_now=True)
-    modified_at = models.DateField(auto_now_add=True)    
+    created_at = models.DateField(auto_now_add=True)
+    modified_at = models.DateField(auto_now=True)    
 
     class Meta:
         abstract = True
