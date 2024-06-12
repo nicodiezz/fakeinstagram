e5f97257f879cb55db972ea1f9ff069733c45cc9
nicodiezz <nicolasagusdiez@gmail.com>
2024-06-12 12:21:42 -0300

    Add search history to session on SearchView at post requests

--- fakeinstagram/home/views.py
+++ fakeinstagram/home/views.py
@@ -1,13 +1,22 @@
+from typing import Any
+from django.http import HttpRequest
+from django.http.response import HttpResponse as HttpResponse
 from django.views.generic import TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from users.models import CustomUser
 # Create your views here.    
 class SearchView(LoginRequiredMixin, TemplateView):
     template_name = "search.html"
-    users = None
     def post(self, request, *args, **kwargs):
+        #Get Results
         key = request.POST.get("key")
-        self.users = CustomUser.objects.filter(username__icontains=key)
+        users = CustomUser.objects.filter(username__icontains=key)
         context = super().get_context_data(**kwargs)
-        context["users"] = self.users
+        context["users"] = users
+        #Store history records
+        data = request.session.session_data.get_decoded()
+        if "search_history" not in data:
+            data["search_history"] = []
+        data["search_history"].append(key)
+                
         return super().render_to_response(context)
