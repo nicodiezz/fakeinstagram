ca20f29a128bbda960a0202ec2f1357e8a0e784a
nicodiezz <nicolasagusdiez@gmail.com>
2024-06-26 17:04:55 -0300

    Add likes list url to posts/urls

--- fakeinstagram/posts/urls.py
+++ fakeinstagram/posts/urls.py
@@ -6,9 +6,11 @@ posts = [
     path('edit-post/<pk>', views.PostUpdateView.as_view(), name="update_post"),
     path('delete-post/<pk>', views.PostDeleteView.as_view(), name="delete_post"),
 ]
+from users.views import LikesListView
 likes=[
     path('like/<pk>', views.LikeCreateView.as_view(), name="like_post"),
-    path('unlike/<pk>', views.LikeDeleteView.as_view(), name="delete_like")
+    path('unlike/<pk>', views.LikeDeleteView.as_view(), name="delete_like"),
+    path("likes/<pk>", LikesListView.as_view(), name="likes")
 ]
 
 urlpatterns= posts+likes
