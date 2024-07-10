9e9a9c12bbf058dd73329225edc4e3c2fa6d9c17
nicodiezz <nicolasagusdiez@gmail.com>
2024-07-10 16:01:56 -0300

    create timesince filter

---
+++ fakeinstagram/home/templatetags/filters.py
@@ -0,0 +1,13 @@
+from django import template
+from django.utils.timesince import timesince
+from django.utils.timezone import now
+from datetime import timedelta
+register = template.Library()
+
+@register.filter(name='timesince')
+def time_since(value):
+    """Dado un datetime, devuelve cuánto tiempo ha pasado desde esa fecha"""  
+    if (now().date() - value).days < 1:
+        return "today"
+    else:
+        return f"{timesince(value, depth=1)} ago"
