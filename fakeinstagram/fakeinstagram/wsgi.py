2c663bfc61f74b8ba6f679e5ef9a44f25b0d150d
nicodiezz <nicolasagusdiez@gmail.com>
2024-07-15 18:47:24 -0300

    fix local wsgi.py

--- fakeinstagram/fakeinstagram/wsgi.py
+++ fakeinstagram/fakeinstagram/wsgi.py
@@ -8,19 +8,9 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 """
 
 import os
-import sys
 
-path = '/home/fakeinstagram/fakeinstagram'
-if path not in sys.path:
-    sys.path.append(path)
-
-# Configura el entorno de Django
-os.environ['DJANGO_SETTINGS_MODULE'] = 'fakeinstagram.settings'
-
-# Activa el entorno virtual
-activate_this = '/home/fakeinstagram/fakeinstagram/env/bin/activate_this.py'
-exec(open(activate_this).read(), dict(__file__=activate_this))
-
-# Importa y activa Django WSGI
 from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fakeinstagram.settings')
+
 application = get_wsgi_application()
