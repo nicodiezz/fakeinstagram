ca30b097922d41f5f4db2d484fc6beac672bd570
nicodiezz <nicolasagusdiez@gmail.com>
2024-08-12 11:27:53 -0300

    Set enviroment variables

--- fakeinstagram/fakeinstagram/settings.py
+++ fakeinstagram/fakeinstagram/settings.py
@@ -9,10 +9,10 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
 import os
 from pathlib import Path
-
+from dotenv import load_dotenv
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -21,11 +21,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-dt)z5vg5dx&7z^ss_v3b+!syc6q=l!(m@^v&1ahq#m69&=(0om'
-
+SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
+DEBUG = os.getenv("DEBUG")=="True"
 ALLOWED_HOSTS = ['prueba123456789.pythonanywhere.com','fakeinstagram.pythonanywhere.com','localhost']
 
 
